b89b7421371df1a7dae41223121452d883e47af8
Jern Kunpittaya <jernjern356@gmail.com>
2021-04-29 04:23:39 -0400

    2nd revised

--- passwordless_pre_Bob/src/App.js
+++ passwordless_pre_Bob/src/App.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 //Here is dev
-import './App.css';
-import WebAuthen from './components/WebAuthen';
+import "./App.css";
+import LocalBob from "./components/LocalBob";
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <WebAuthen />
+        <LocalBob />
       </div>
-);
+    );
   }
 }
 export default App;
